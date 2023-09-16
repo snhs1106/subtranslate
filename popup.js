188934230af5a13aef6aa93fb14b323c24eb09b8
snhs1106 <stephanieh0611@gmail.com>
2023-09-16 16:18:39 -0400

    changes made to js

--- popup.js
+++ popup.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const grabUrlButton = document.getElementById('grabUrl');
   const urlDisplay = document.getElementById('urlDisplay');
+  const test = document.getElementById('grabUrl')
+
+  test.addEventListener(
+    "mouseenter",
+    (event) => {
+      event.target.style.color = "purple";
+      // setTimeout(())
+    }
+  )
 
   grabUrlButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -10,3 +19,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+
+
