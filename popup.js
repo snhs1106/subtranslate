80198cd81676838d886a7f563d34a07fdf9d813b
snhs1106 <stephanieh0611@gmail.com>
2023-09-16 17:03:19 -0400

    quick inline css

--- popup.js
+++ popup.js
@@ -1,16 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const grabUrlButton = document.getElementById('grabUrl');
   const urlDisplay = document.getElementById('urlDisplay');
-  const test = document.getElementById('grabUrl')
-
-  test.addEventListener(
-    "mouseenter",
-    (event) => {
-      event.target.style.color = "purple";
-      // setTimeout(())
-    }
-  )
-
+  
   grabUrlButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       const tab = tabs[0];
@@ -18,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
       urlDisplay.textContent = url;
     });
   });
+
 });
-
-
 
